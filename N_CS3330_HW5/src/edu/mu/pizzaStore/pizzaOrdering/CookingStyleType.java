572c4e4e49909a6b8a049979a63e8cd4851035a9
DukeMinty <atorby123@gmail.com>
2024-03-02 22:35:53 -0600

    Finished paramaters for CookingStyleType enum

--- N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaOrdering/CookingStyleType.java
+++ N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaOrdering/CookingStyleType.java
@@ -1,5 +1,13 @@
 package edu.mu.pizzaStore.pizzaOrdering;
 
-public enum CookingStyleType {
+//THIS ENUM IS FINISHED
 
+//Student: Aaron Trebus
+
+//This enum declares the types of cooking styles available
+
+public enum CookingStyleType {
+	MICROWAVE,
+	CONVENTIONAL_OVEN,
+	BRICK_OVEN
 }
