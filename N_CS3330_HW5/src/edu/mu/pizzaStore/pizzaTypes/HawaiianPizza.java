e949bfcef8dfb34404c89a4d8847a7a73f1e2efe
DukeMinty <atorby123@gmail.com>
2024-03-03 15:23:16 -0600

    Created HawaiianPizza constructor and its inherited copy constructor

--- N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaTypes/HawaiianPizza.java
+++ N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaTypes/HawaiianPizza.java
@@ -1,5 +1,39 @@
 package edu.mu.pizzaStore.pizzaTypes;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.mu.pizzaStore.cooking.ICookingStrategy;
+import edu.mu.pizzaStore.pizzaOrdering.Toppings;
+
 public class HawaiianPizza extends AbstractPizza{
+	
+	//Arraylist that defines the default toppings for a Hawaiian Pizza
+	protected final static List<Toppings> defaultToppings = new ArrayList<>(List.of(Toppings.CHEESE, Toppings.CANADIAN_BACON, Toppings.PINEAPPLE));
+	
+	public HawaiianPizza(double totalPrice,double cookingPrice, int pizzaOrderID, ICookingStrategy cookingStrategy) {
+		super(totalPrice,cookingPrice,pizzaOrderID,cookingStrategy);
+		//The default toppings are added to the toppingList
+		toppingList.addAll(defaultToppings);
+		setPriceWithoutToppings(3.00);
+	}
+
+	//copy constructor
+	public HawaiianPizza(HawaiianPizza original) {
+		super(original);
+	}
+
+	@Override
+	protected double addToppingsToPrice(double riceWithoutToppings) {
+		// TODO Auto-generated method stub
+		return super.addToppingsToPrice(priceWithoutToppings);
+	}
+
+	@Override
+	protected double updatePizzaPrice() {
+		// TODO Auto-generated method stub
+		return super.updatePizzaPrice();
+	}
+	
 
 }
