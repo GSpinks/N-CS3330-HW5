336a5984f10bab61873eb58ab904c34040821234
DukeMinty <atorby123@gmail.com>
2024-03-04 10:35:04 -0600

    Added the cook method to ICookingStrategy interface (finished interface)

--- N_CS3330_HW5/src/edu/mu/pizzaStore/cooking/ICookingStrategy.java
+++ N_CS3330_HW5/src/edu/mu/pizzaStore/cooking/ICookingStrategy.java
@@ -1,5 +1,17 @@
 package edu.mu.pizzaStore.cooking;
+import edu.mu.pizzaStore.pizzaTypes.AbstractPizza;
+
+/*
+ * Student: Aaron Trebus
+ * 
+ * This Interface is finished.
+ * 
+ * The implemented method will be overwritten by the three
+ * cooking style classes
+ */
 
 public interface ICookingStrategy {
+	
+	public boolean cook(AbstractPizza pizza);
 
 }
