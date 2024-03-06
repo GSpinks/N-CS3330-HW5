2211ba16596ea08106a981de804843e863fad2c3
DukeMinty <atorby123@gmail.com>
2024-03-06 16:48:27 -0600

    Made constructor how VeggiePizza class and the necess. getter

--- N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaTypes/VegetarianPizza.java
+++ N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaTypes/VegetarianPizza.java
@@ -7,38 +7,39 @@ import edu.mu.pizzaStore.pizzaOrdering.Toppings;
 
 public class VegetarianPizza extends AbstractPizza{
 	
+	/*
+	 * Student: Aaron Trebus
+	 * 
+	 * This class makes the constructors for all supremePizza objects.
+	 */
+	
 	//Arraylist that defines the default toppings for a Supreme Pizza
 	protected final static List<Toppings> defaultToppings = new ArrayList<>(List.of(Toppings.TOMATO, Toppings.CHEESE,
 			Toppings.BELL_PEPPER, Toppings.BLACK_OLIVE, Toppings.MUSHROOM));
 	
-	/*TODO
-	 * 
-	 * I don't know how to make the constructors for these classes.
-	 * If anyone else could work on this part, I'd appreciate it.
-	 * 
-	 * Prof said the subclasses should use default constructors.
+	/*
 	 * Above is the list of toppings to add to a veggie pizza's toppingList on construction
 	 * A hawaiian pizza's default price is 1.50
 	 * 
-	 * The constructor may have to call on user input in order to be finalized,
-	 * in which case we can save it until we're done with the cookingStrategy Interface
-	 * and the pizzaOrder methods
+	 * The constructor will make a pizza, but we'll user input in order to make a complete pizza,
+	 * with attributes of totalPrice, cookingStrategy, and cookingPrice. I'll
+	 * make an example of this in main.
 	 */
-
-	@Override
-	protected double addToppingsToPrice(double priceWithoutToppings) {
-		// TODO
-		//see hawaiianpizza for notes
-		return super.addToppingsToPrice(priceWithoutToppings);
+	
+	public VegetarianPizza() {
+		super();
+		this.toppingList.addAll(defaultToppings);
+		this.priceWithoutToppings = 1.50;
+		this.pizzaOrderID = orderIDCounter++;
 	}
 
-	@Override
-	protected double updatePizzaPrice() {
-		// TODO 
-		//See hawaiianpizza for notes
-		return super.updatePizzaPrice();
+	//copy constructor
+	public VegetarianPizza(VegetarianPizza original) {
+		super(original);
 	}
-	
-	
+
+	public void callAddToppingsToPrice(double priceWithoutToppings) {
+		super.addToppingsToPrice(priceWithoutToppings);
+	}
 
 }
