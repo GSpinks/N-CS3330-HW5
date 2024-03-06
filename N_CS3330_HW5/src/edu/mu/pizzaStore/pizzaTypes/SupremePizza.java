e72bcad73d415cce71c595489c84190f8ce6948e
DukeMinty <atorby123@gmail.com>
2024-03-06 16:48:16 -0600

    Made constructor how SupremePizza class and the necess. getter

--- N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaTypes/SupremePizza.java
+++ N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaTypes/SupremePizza.java
@@ -7,39 +7,38 @@ import edu.mu.pizzaStore.pizzaOrdering.Toppings;
 
 public class SupremePizza extends AbstractPizza{
 	
+	/*
+	 * Student: Aaron Trebus
+	 * 
+	 * This class makes the constructors for all supremePizza objects.
+	 */
+	
 	//Arraylist that defines the default toppings for a Supreme Pizza
-		protected final static List<Toppings> defaultToppings = new ArrayList<>(List.of(Toppings.TOMATO, Toppings.CHEESE,
+	protected final static List<Toppings> defaultToppings = new ArrayList<>(List.of(Toppings.TOMATO, Toppings.CHEESE,
 				Toppings.BELL_PEPPER, Toppings.ITALIAN_SAUSAGE, Toppings.PEPPERONI, Toppings.BLACK_OLIVE, Toppings.MUSHROOM));
 		
-		/*TODO
-		 * 
-		 * I don't know how to make the constructors for these classes.
-		 * If anyone else could work on this part, I'd appreciate it.
-		 * 
-		 * Prof said the subclasses should use default constructors.
+		/*
 		 * Above is the list of toppings to add to a supreme pizza's toppingList on construction
 		 * A hawaiian pizza's default price is 3.50
 		 * 
-		 * The constructor may have to call on user input in order to be finalized,
-		 * in which case we can save it until we're done with the cookingStrategy Interface
-		 * and the pizzaOrder methods
+		 * The constructor will make a pizza, but we'll user input in order to make a complete pizza,
+		 * with attributes of totalPrice, cookingStrategy, and cookingPrice. I'll
+		 * make an example of this in main.
 		 */
 		
-		@Override
-		protected double addToppingsToPrice(double priceWithoutToppings) {
-			// TODO 
-			//See hawaiianpizza for notes
-			return super.addToppingsToPrice(priceWithoutToppings);
-		}
+	public SupremePizza() {
+	super();
+	this.toppingList.addAll(defaultToppings);
+	this.priceWithoutToppings = 3.50;
+	this.pizzaOrderID = orderIDCounter++;
+	}
 
-		@Override
-		protected double updatePizzaPrice() {
-			// TODO
-			//See hawaiianpizza for notes
-			return super.updatePizzaPrice();
-		}
-		
-	
-		
+	//copy constructor
+	public SupremePizza(SupremePizza original) {
+		super(original);
+	}
 
+	public void callAddToppingsToPrice(double priceWithoutToppings) {
+		super.addToppingsToPrice(priceWithoutToppings);
+	}
 }
