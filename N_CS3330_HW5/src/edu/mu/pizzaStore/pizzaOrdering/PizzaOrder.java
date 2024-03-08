702731687f6918727e3f3b8e0621abc0bc57cff6
Christian Kiner <christiankiner21@icloud.com>
2024-03-07 19:54:00 -0600

    Created the printListOfToppingsByPizzaOrderID method
    This method searches the order list for the given orderID and prints the toppings.

--- N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaOrdering/PizzaOrder.java
+++ N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaOrdering/PizzaOrder.java
@@ -15,7 +15,31 @@ public class PizzaOrder {
 		pizzaFactory = new PizzaCookingFactory();
 		pizzaOrderList = new ArrayList<AbstractPizza>();
 	}
-	
+
+	public void printListOfToppingsByPizzaOrderID(int orderID) {
+		// Gets the pizza order associated with orderID and prints the toppings associated if the order exists.
+		boolean orderExists = false;
+		
+		for (AbstractPizza pizza : pizzaOrderList) {
+			// Order found
+			if (pizza.getPizzaOrderID() == orderID) {
+				orderExists = true;
+				
+				System.out.println("The toppings on order " + Integer.toString(orderID) + " are:");
+				// Iterate through toppings and print them one by one. This is done for formatting reasons.
+				for (Toppings t : pizza.getToppingList()) {
+					System.out.printf("\t%s\n", t.toString());
+				}
+				
+				break;  // Order found.
+			}
+		}
+		
+		// Did not find order
+		if (!orderExists) {
+			System.out.println("Order #" + Integer.toString(orderID) + " does not exist.");
+		}
+	}
 	
 	
 }
