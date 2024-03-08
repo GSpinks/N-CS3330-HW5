a15952798650f69c8f25cf4c83b62a6e87f32722
Christian Kiner <christiankiner21@icloud.com>
2024-03-07 19:51:52 -0600

    Created the attributes and constructor for the PizzaOrder class.

--- N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaOrdering/PizzaOrder.java
+++ N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaOrdering/PizzaOrder.java
@@ -1,5 +1,21 @@
 package edu.mu.pizzaStore.pizzaOrdering;
 
-public class PizzaOrder {
+import edu.mu.pizzaStore.cooking.*;
+import edu.mu.pizzaStore.pizzaTypes.*;
+import java.util.ArrayList;
+import java.util.List;
 
+public class PizzaOrder {
+	private PizzaCookingFactory pizzaFactory;
+	private ICookingStrategy cookingStrategy;
+	private List<AbstractPizza> pizzaOrderList;
+	
+	// Default Constructor
+	public PizzaOrder() {
+		pizzaFactory = new PizzaCookingFactory();
+		pizzaOrderList = new ArrayList<AbstractPizza>();
+	}
+	
+	
+	
 }
