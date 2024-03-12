a0403574538431ea2c8a72e452babe7c3ff0b7b1
GSpinks <111899656+GSpinks@users.noreply.github.com>
2024-03-12 15:06:24 -0500

    created the create Pizza method which creates a unique pizza order id

--- N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaOrdering/PizzaCookingFactory.java
+++ N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaOrdering/PizzaCookingFactory.java
@@ -1,5 +1,27 @@
 package edu.mu.pizzaStore.pizzaOrdering;
 
-public class PizzaCookingFactory {
+import edu.mu.pizzaStore.pizzaTypes.AbstractPizza;
+import edu.mu.pizzaStore.pizzaTypes.HawaiianPizza;
+import edu.mu.pizzaStore.pizzaTypes.MargheritaPizza;
+import edu.mu.pizzaStore.pizzaTypes.SupremePizza;
+import edu.mu.pizzaStore.pizzaTypes.VegetarianPizza;
 
+public class PizzaCookingFactory {
+  
+  static int orderIDcounter = 0;
+	public AbstractPizza createPizza(PizzaType pizzaType) {
+	        AbstractPizza pizza = null;
+	        if(pizzaType.equals(PizzaType.HAWAIIAN)) {
+	        	pizza = new HawaiianPizza();
+	        }else if(pizzaType.equals(PizzaType.MARGHERITA)) {
+	        	pizza = new MargheritaPizza();
+	        } else if(pizzaType.equals(PizzaType.SUPREME)) {
+	        	pizza = new SupremePizza();
+	        }else if(pizzaType.equals(PizzaType.VEGETARIAN)) {
+	        	pizza = new VegetarianPizza();
+	        }
+			orderIDcounter ++;
+			pizza.setPizzaOrderID(orderIDcounter);
+			return pizza;
+	 }
 }
