171b646074fdd87b3344b0c2a01bc1e3d6c8ab16
GSpinks <111899656+GSpinks@users.noreply.github.com>
2024-03-11 19:15:39 -0500

    Implemented createPizza method which assigns a pizza created to a unique order id

--- N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaOrdering/PizzaCookingFactory.java
+++ N_CS3330_HW5/src/edu/mu/pizzaStore/pizzaOrdering/PizzaCookingFactory.java
@@ -1,5 +1,32 @@
 package edu.mu.pizzaStore.pizzaOrdering;
 
-public class PizzaCookingFactory {
+import edu.mu.pizzaStore.pizzaTypes.AbstractPizza;
+import edu.mu.pizzaStore.pizzaTypes.HawaiianPizza;
+import edu.mu.pizzaStore.pizzaTypes.MargheritaPizza;
+import edu.mu.pizzaStore.pizzaTypes.SupremePizza;
+import edu.mu.pizzaStore.pizzaTypes.VegetarianPizza;
 
+public class PizzaCookingFactory {
+  static int PizzaOrderID = 0;
+	
+	public AbstractPizza createPizza(PizzaType pizzaType){
+		AbstractPizza kindOfPizza = null;
+		
+		if(pizzaType.equals((PizzaType.HAWAIIAN))){
+			kindOfPizza = new HawaiianPizza();
+		}
+		else if(pizzaType.equals((PizzaType.MARGHERITA))){
+			kindOfPizza = new MargheritaPizza();
+		}
+		else if(pizzaType.equals((PizzaType.VEGETARIAN))){
+			kindOfPizza = new VegetarianPizza();
+		}
+		else if(pizzaType.equals((PizzaType.SUPREME))){
+			kindOfPizza = new SupremePizza();
+		}
+		
+		PizzaOrderID++;
+		kindOfPizza.setPizzaOrderID(PizzaOrderID);
+		return kindOfPizza;
+	}
 }
